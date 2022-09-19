75393a8694923d11fca0da7391db69f138823f3b
Никита Фантокин <Fantokin03@mail.ru>
2022-09-19 23:22:17 +0300

    V 0.9

--- cs-labs-console/Labs/Lab3.cs
+++ cs-labs-console/Labs/Lab3.cs
@@ -12,35 +12,41 @@ namespace CSharpLabs.Labs
     {
         public void Run()
         {
-
             Task1();
-
         }
         // -------------------------------------------------| Задание 1 |------------------------------------------------------
         private static void Task1()
         {
-
-            Console.WriteLine("Введите номер задания");
             int ask = default(int) - 1;
+            
 
             while (ask != 0)
             {
+                Console.Clear();
+                Console.WriteLine("Введите номер задания\n2) Задание 2\n3) Задание 3\n4) Задание 4\n5) Выход");
+                
+                try { ask = Convert.ToInt32(Console.ReadLine()); }
+                catch { continue; }
+
                 switch (ask)
                 {
-                    case 1:
+                    case 2:
                         Task2();
                         break;
-                    case 2:
-
-                        break;
                     case 3:
-
+                        Task3();
                         break;
                     case 4:
 
                         break;
+                    case 5:
+
+                        break;
+                    default: continue;
                 }
             }
+
+            Console.ReadKey();
         }
 
         // -------------------------------------------------| Задание 2 |------------------------------------------------------
@@ -48,7 +54,6 @@ namespace CSharpLabs.Labs
         private static void Task2()
         {
             Console.WriteLine("Введите количество чисел:");
-            Console.Clear();
 
             int amount = int.Parse(Console.ReadLine()!);
 
@@ -62,6 +67,21 @@ namespace CSharpLabs.Labs
                 if (input[i].ToString().Length == 3)
                 {
                     count++;
+                }
+            }
+
+            if (count == 0) Console.WriteLine("Среди заданных чисел нет трёхзначных");   
+            else Console.WriteLine($"Количество чисел удовлетворяющих условие: {count}");
+        }
+        private static void Task3()
+        {
+            var output = new List<int>();
+
+            for (int i = 1000; i < 9999; i++)
+            {
+                if (i % 133 == 125 && i % 134 == 111)
+                {
+                    Console.WriteLine(i);
                 }
             }
         }
